f84d819e4a42fb3a7baa7ae9f6126688746fb2c9
luwat <oluwatobioreniyi@gmail.com>
2025-02-14 17:18:11 +0100

    Implement dynamic data fetching for acquisitions and dimensions charts

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,29 +1,47 @@
-import Chart from 'chart.js/auto'
+import {
+  Chart,
+  Colors,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Legend,
+} from "chart.js";
+import { getAquisitionsByYear } from "./api";
 
-(async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+Chart.register(
+  Colors,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Legend
+);
 
-  new Chart(
-    document.getElementById('acquisitions'),
-    {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-          }
-        ]
-      }
-    }
-  );
+(async function () {
+  const data = await getAquisitionsByYear();
+
+  new Chart(document.getElementById("acquisitions"), {
+    type: "bar",
+    options: {
+      animation: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+        },
+      },
+    },
+    data: {
+      labels: data.map((row) => row.year),
+      datasets: [
+        {
+          label: "Acquisitions by year",
+          data: data.map((row) => row.count),
+        },
+      ],
+    },
+  });
 })();
